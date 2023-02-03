f1fa7494d4fe37af20f0cd9ddc3c2a2e060791eb
nerkat <nerkat@gmail.com>
2023-02-03 13:42:23 +0700

    clean

--- scripts/content.js
+++ scripts/content.js
@@ -1,50 +1,52 @@
-// Description: This script is injected into the page and runs in the context of the page. It can access the DOM and make changes to the page.
+// Content Script for Chrome Extension to copy preview link of Shopify theme
 // Documentation: https://developer.chrome.com/extensions/content_scripts
 
-var isShopify = document.querySelector('meta[name="shopify-digital-wallet"]'); // check if shopify page
+let button;
+// Check if the current page is a Shopify page
+const isShopify = document.querySelector('meta[name="shopify-digital-wallet"]');
 if (isShopify) {
+
+  // Wait for the preview bar to load
   setTimeout(() => {
+    const previewBarIframe = document.querySelector('#preview-bar-iframe');
+    if (previewBarIframe) {
 
-    const previewUrl = document.querySelector('#preview-bar-iframe').src;
-    // load the preview bar url and get data from dom
-    fetch(previewUrl)
-      .then(response => response.text())
-      .then(data => {
+      const previewUrl = previewBarIframe.src;
 
-        // parse the html and get the theme name and preview link
-        const parser = new DOMParser();
-        const htmlDoc = parser.parseFromString(data, 'text/html');
-        const previewLink = htmlDoc.querySelector('#share_theme_url').value;
-        const themeName = htmlDoc.querySelector('.ui-type-container.ui-type-container--spacing-extra-tight p strong').innerHTML;
+      // Fetch the preview bar HTML and extract the preview link and theme name
+      fetch(previewUrl)
+        .then(response => response.text())
+        .then(data => {
+          const parser = new DOMParser();
+          const htmlDoc = parser.parseFromString(data, 'text/html');
+          const previewLink = htmlDoc.querySelector('#share_theme_url').value;
+          const themeName = htmlDoc.querySelector('.ui-type-container.ui-type-container--spacing-extra-tight p strong').innerHTML;
 
-
-        //create button and add to page
-        const buttonURL = document.createElement('a');
-        buttonURL.innerHTML = 'Copy URL';
-        buttonURL.style = `
+          // Create a "Copy URL" button and add it to the page
+          button = document.createElement('a');
+          button.innerHTML = 'Copy URL';
+          button.style = `
         position: fixed;
         bottom: 10px;
+        font-size: 10px;
         right: 367px;
         z-index: 2147483647;
         color: black;
         padding: 10px;
         border: none;
         cursor: pointer;
-        `;
-
-        buttonURL.onclick = () => {
-          // copy link to Clipboard
-          html2clipboard("<a href='" + previewLink + "'>" + themeName + "</a>");
-        };
-
-        document.body.appendChild(buttonURL);
-
-      });
-
+      `;
+          button.onclick = () => {
+            copyLinkToClipboard(`<a href='${previewLink}'>${themeName}</a>`);
+          };
+          document.body.appendChild(button);
+        });
+    }
   }, 500);
 }
 
-function html2clipboard(content) {
+// Copy the given content to the clipboard
+function copyLinkToClipboard(content) {
   const copyListener = event => {
     event.clipboardData.setData('text/plain', content);
     event.clipboardData.setData('text/html', content);
@@ -54,6 +56,12 @@ function html2clipboard(content) {
   document.addEventListener('copy', copyListener);
   document.execCommand('copy');
   document.removeEventListener('copy', copyListener);
+
+  // Change the button text to "Copied"
+  button.innerHTML = 'Copied';
+
+  // Change the button text back to "Copy URL" after 2 seconds
+  setTimeout(() => {
+    button.innerHTML = 'Copy URL';
+  }, 2000);
 }
-
-
