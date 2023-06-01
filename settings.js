46304ed4afa25edd6a676e28e9e040c39a67108a
nerkat <nerkat@gmail.com>
2023-06-01 17:29:16 +0700

    V1

--- settings.js
+++ settings.js
@@ -1,19 +1,11 @@
 // get settings 
 chrome.storage.sync.get('urlButton', function (data) {
   document.getElementById('urlButton').checked = data.urlButton;
-  document.getElementById('TWDarkTheme').checked = data.TWDarkTheme;
 });
 
 // save 'urlButton' to extension settings
 document.getElementById('urlButton').addEventListener('change', function () {
   chrome.storage.sync.set({
     urlButton: this.checked
-  });
-});
-
-// save 'TWDarkTheme' to extension settings
-document.getElementById('TWDarkTheme').addEventListener('change', function () {
-  chrome.storage.sync.set({
-    TWDarkTheme: this.checked
   });
 });
